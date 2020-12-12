aa7e2523a6c70c5ce6ca69f00280e46f2334c53d
Daniel Barclay <barclaysd@me.com>
2020-12-12 12:18:38 +0000

    Rename SquareComponet => Square

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Board } from './board/board.component';
-import { SquareComponent } from './square/square.component';
+import { Square } from './square/square.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Board,
-    SquareComponent
+    Square
   ],
   imports: [
     BrowserModule,
